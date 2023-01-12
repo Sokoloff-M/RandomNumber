028484ec7f0bb9a984489c3b6f06441e426eebd8
Sokoloff-M <109232419+Sokoloff-M@users.noreply.github.com>
2023-01-12 18:31:56 +0800

    Программа "Угадай число"

---
+++ main.py
@@ -0,0 +1,12 @@
+from  random import randint
+
+rr = randint(0, 10)
+
+a = int(input())
+while a == rr:
+    if a == rr:
+        print("Вы верно угадали число")
+
+    else:
+        print("Не верно, попробуйте еще раз")
+
---
+++ rr.py
@@ -0,0 +1,10 @@
+from random import randint as rr
+
+a = rr(0, 10)
+
+while True:
+    b = int(input())
+    if a == b:
+        print('wery god')
+    else:
+        print('oh... fuck')
